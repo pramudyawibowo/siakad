a659a3eea8a26f17b7ee79989c5f2eb3301a560c
ahmadfahry <ahmadfahrysk8@gmail.com>
2022-01-05 19:22:37 +0700

    commit on master

---
+++ app/Http/Controllers/APIcontroller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class APIcontroller extends Controller
+{
+    ////Function untuk Login
+    function loginAndroid(Request $request) {
+        $logins = DB::table('login')
+         ->where('email', $request->email)
+         ->where('password', $request->password)
+         ->get();
+      
+        if (count($logins) > 0) {
+         foreach ($logins as $logg) {
+          $result["success"] = "1";
+          $result["message"] = "success";
+          //untuk memanggil data sesi Login
+          $result["id"] = $logg->id;
+          $result["nama"] = $logg->nama;
+          $result["nim"] = $logg->nim;
+          $result["email"] = $logg->email;
+         }
+         echo json_encode($result);
+        } else {
+         $result["success"] = "0";
+         $result["message"] = "error";
+         echo json_encode($result);
+        }
+       }
+}
